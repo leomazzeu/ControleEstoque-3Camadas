11d2db387fd31065bfa0b6fd784b206cd05c0252
leomazzeu <leonardomazzeudasilva@gmail.com>
2023-03-27 22:44:09 -0300

    Mapeamento das entidades do banco de dados

---
+++ src/CE.BUSINESS/Models/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CE.BUSINESS.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        // Relação EF
+        public IEnumerable<Produto> Produtos { get; set; } = Enumerable.Empty<Produto>();
+    }
+}
